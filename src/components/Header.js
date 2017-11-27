1b4c6d52fdd79e5aa057513a37665a5b57c83226
Rafael Escudeiro <rafael-tome@hotmail.com>
2017-11-27 00:22:06 +0000

    Team Component & Header Update
    Create Team Component & Header Update (Create button & Language Flags)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import { Flag, Button, Icon } from 'semantic-ui-react';
+
 
 import '../styles/header.css';
 
@@ -12,10 +14,17 @@ export default class Header extends React.Component{
                         <li>Logo</li>
                         <li><Link to='/home'>Home</Link></li>
                         <li><Link to='/game/create'>Create Game</Link></li>
+
                     </ul>
                     <ul className="nav-right">
                         <li><Link to='/login'>Login</Link></li>
                         <li><Link to='/register'>Register</Link></li>
+                        <Button icon>
+                            <Flag name='pt'/>
+                        </Button>
+                        <Button icon>
+                        <Flag name='gb'/>
+                        </Button>
                     </ul>
                 </div>
             </header>
