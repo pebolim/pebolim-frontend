0e98bd4a31e881f9801898cbeb1ef353f1719178
davidbernardo <ravageiro@hotmail.com>
2017-12-01 18:15:22 +0000

    Create game style changes

--- src/components/Games/CreateGame.js
+++ src/components/Games/CreateGame.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import { Checkbox, Dropdown, Input } from 'semantic-ui-react';
+import { Checkbox, Dropdown, Input, Button, Icon, Grid } from 'semantic-ui-react';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -12,28 +12,42 @@ export default class CreateGame extends React.Component {
         this.state = {
             local: '',
             isPrivate: '',
-            hour:'',
-            minutes:'',
+            hour: '',
+            minutes: '',
             startDate: moment()
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);      
-        this.handleTimeDropdown = this.handleTimeDropdown.bind(this);      
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleTimeHours = this.handleTimeHours.bind(this);
+        this.handleTimeMinutes = this.handleTimeMinutes.bind(this);
     }
-    
-    handleTimeDropdown(e, data){
+
+    handleTimeHours(e, data) {
         e.preventDefault();
         this.setState(
-            { 
-                hour: data.value 
+            {
+                hour: data.value
             }, function () {
-                console.log("State:"+ this.state.hour);
+                console.log("State:" + this.state.hour);
             }.bind(this)
         );
 
-        console.log("Input value:"+ data.value);
+        console.log("Input value:" + data.value);
+    }
+
+    handleTimeMinutes(e, data) {
+        e.preventDefault();
+        this.setState(
+            {
+                minutes: data.value
+            }, function () {
+                console.log("State:" + this.state.hour);
+            }.bind(this)
+        );
+
+        console.log("Input value:" + data.value);
     }
 
     handleInputChange(event) {
@@ -42,9 +56,9 @@ export default class CreateGame extends React.Component {
         const name = target.name;
 
         this.setState({
-          [name]: value
+            [name]: value
         });
-      }    
+    }
 
     handleChange(date) {
         this.setState({
@@ -58,46 +72,82 @@ export default class CreateGame extends React.Component {
         fetch('https://127.0.0.1:3002/game/', {
             method: 'POST',
             headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(this.state)
-          }) 
-      }
-      
+        })
+    }
+
     render() {
         return (
             <div>
-                <h1>Criar Jogo</h1>
+                <Grid container>
+                    <Grid.Row>
+                        <h1>Criar Jogo</h1>
+                    </Grid.Row>
+                    <Grid.Row columns='equal'>
+                        <Grid.Column>
+                            <div>
+                                <h3>Localização: </h3>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column width={14}>
+                            <Input fluid value={this.state.local} name="local" onChange={this.handleInputChange} icon='marker' placeholder='Localização do jogo...' />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row columns={8}>
+                        <Grid.Column>
+                            <div>
+                                <h3>Data do jogo: </h3>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <DatePicker
+                                selected={this.state.startDate}
+                                onChange={this.handleChange}
+                                name="startDate"
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row columns={8}>
+                        <Grid.Column>
+                            <div>
+                                <h3>Hora do jogo: </h3>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Dropdown fluid value={this.state.hour} name="hour" onChange={this.handleTimeHours} placeholder='Horas' selection options={getHours()} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Dropdown fluid value={this.state.minutes} name="minutes" onChange={this.handleTimeMinutes} placeholder='Minutos' selection options={getMinutes()} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div>
+                                <h3>Privado: </h3>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="ui fitted toggle checkbox">
+                                <input type="checkbox" name="isPrivate" onChange={this.handleInputChange} readOnly="" tabIndex="0" />
+                                <label></label>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
 
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <p>Localização:</p>
-                        <Input value={this.state.local} name="local" onChange={this.handleInputChange} icon='marker' placeholder='Localização do jogo...'/>
-                    </div>
-                    <div>
-                        <DatePicker
-                            selected={this.state.startDate}
-                            onChange={this.handleChange}
-                            name="startDate"
-                        />
-                    </div>
-                    <div>
-                        <Dropdown value={this.state.hour} onChange={this.handleTimeDropdown} placeholder='Horas' selection options={getHours()} />
-                        <Dropdown value={this.state.minutes} name="minutes" onChange={this.handleInputChange} placeholder='Minutos' selection options={getMinutes()} />
-                    </div>
-                    <div>
-                        <p>Privado</p>
-                        <div className="ui fitted toggle checkbox">
-                            <input type="checkbox" name="isPrivate" onChange={this.handleInputChange} readOnly="" tabIndex="0" />
-                            <label></label>
-                        </div>
-                    </div>
-
-                    <div>
-                        <input type="submit" value="Submit" />
-                    </div>
-                </form>
+                    <Grid.Row columns={5}>
+                        <Grid.Column>
+                            <Button fluid animated onClick={this.handleSubmit}>
+                                <Button.Content visible>Next</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name='right arrow' />
+                                </Button.Content>
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         );
     }
