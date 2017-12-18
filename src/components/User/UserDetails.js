1e4f75cc8813a45a30c6cef2498717c5033324f4
ricardma <aluno18774@ipt.pt>
2017-12-18 16:15:44 +0000

    GamesByUser

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Time from 'react-time-format';
-import { Container, Header, Grid, Image, Label, Icon } from 'semantic-ui-react';
+import { Container, Header, Grid, Image, Label, Icon, Loader, Dimmer } from 'semantic-ui-react';
 import GamesByUser from '../Games/GamesByUser'
 
 export default class UserDetails extends React.Component {
@@ -9,7 +9,8 @@ export default class UserDetails extends React.Component {
 
         this.state = {
             user: {
-            }
+            },
+            loading: true
         };
     }
 
@@ -25,39 +26,49 @@ export default class UserDetails extends React.Component {
         fetch(`http://localhost:3000/player`, myInit)
             .then(result => result.json())
             .then(usr => this.setState({ user: usr.user }))
+        this.setState({loading:false})
 
     }
 
     render() {
-        return (
-            <div>
-                <Header id="title" style={{ textAlign: 'left', paddingBottom:20, fontSize:50}}>{this.state.user.nickname}'s profile</Header>
-                <Grid>
-                    <Grid.Column width={4} style={{ backgroundColor: '#353535' }}>
-                        <Image src={require('../../assets/images/datboy.png')} centered />
-                        <div style={{ fontSize: 30, textAlign: 'center', fontWeight: 600, paddingTop: 10 }}>
-                            {this.state.user.nickname}
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={4} style={{paddingLeft:30}}>
-                        <Grid.Row style={{ fontSize: 25,fontWeight: 600, paddingBottom: 10 }}>
-                            Email:
+        const loading = this.state.loading;
+        if (loading) {
+            return (
+                <Dimmer active>
+                    <Loader size='massive'>Loading</Loader>
+                </Dimmer>
+            )
+        } else {
+            return (
+                <div>
+                    <Header id="title" style={{ textAlign: 'left', paddingBottom: 20, fontSize: 50 }}>{this.state.user.nickname}'s profile</Header>
+                    <Grid>
+                        <Grid.Column width={4} style={{ backgroundColor: '#353535' }}>
+                            <Image src={require('../../assets/images/datboy.png')} centered />
+                            <div style={{ fontSize: 30, textAlign: 'center', fontWeight: 600, paddingTop: 10 }}>
+                                {this.state.user.nickname}
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column width={4} style={{ paddingLeft: 30 }}>
+                            <Grid.Row style={{ fontSize: 25, fontWeight: 600, paddingBottom: 10 }}>
+                                Email:
                         </Grid.Row>
-                        <Grid.Row style={{ fontSize: 18, paddingBottom: 30, paddingLeft:5 }}>
-                            {this.state.user.email}
+                            <Grid.Row style={{ fontSize: 18, paddingBottom: 30, paddingLeft: 5 }}>
+                                {this.state.user.email}
+                            </Grid.Row>
+                            <Grid.Row style={{ fontSize: 25, fontWeight: 600, paddingBottom: 10 }}>
+                                Age:
                         </Grid.Row>
-                        <Grid.Row style={{ fontSize: 25,fontWeight: 600, paddingBottom: 10 }}>
-                            Age:
-                        </Grid.Row>
-                        <Grid.Row style={{ fontSize: 18, paddingBottom: 30, paddingLeft:5 }}>
-                            {this.state.user.age}
-                        </Grid.Row>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <GamesByUser />
-                    </Grid.Column>
-                </Grid>
-            </div>
-        );
+                            <Grid.Row style={{ fontSize: 18, paddingBottom: 30, paddingLeft: 5 }}>
+                                {this.state.user.age}
+                            </Grid.Row>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <GamesByUser />
+                        </Grid.Column>
+                    </Grid>
+                </div>
+            );
+        }
     }
 }
